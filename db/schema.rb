f70fe9f9c7a6d941713a3f25bbb06825c8a4037d
mikhail <yurkovetc@gmail.com>
2016-11-30 16:59:21 +0300

    Added videos

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117200221) do
+ActiveRecord::Schema.define(version: 20161130124237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string   "title"
+    t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 5.times do |i|
   Post.create(title: "Post_#{i}")
 end
+5.times do |i|
+  Video.create(title: "Video_#{i}", url: "Url#{i}")
+end
