5f2d9c3d0b2c2f8f075ed863aa2abcf03362b0c2
mikhail <yurkovetc@gmail.com>
2016-12-03 19:22:59 +0300

    Embeded videos on show page

--- app/videos/video-detail.component.ts
+++ app/videos/video-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
+import { BrowserModule }            from '@angular/platform-browser'
+import { DomSanitizer }             from "@angular/platform-browser";
 
 import { Video } from './video';
 import { VideoService } from './video.service';
@@ -14,16 +16,21 @@ import 'rxjs/add/operator/switchMap';
   styleUrls: ['video-detail.component.css']*/
 })
 export class VideoDetailComponent implements OnInit {
+  videoUrl;
   constructor(
     private videoService: VideoService,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private domSanitizer : DomSanitizer
   ) {}
 
   ngOnInit(): void {
     this.route.params
       .switchMap((params: Params) => this.videoService.getVideo(+params['id']))
-      .subscribe(video => this.video = video);
+      .subscribe(video => {
+        this.video = video;
+        this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.video.url);
+      });
   }
   goBack(): void {
     this.location.back();
