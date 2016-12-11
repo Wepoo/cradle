582f021aca6adf5897557b912fc4b9bf40d00ab8
mikhail <yurkovetc@gmail.com>
2016-12-11 14:20:57 +0300

    Added description to videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -46,6 +46,6 @@ class VideosController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def video_params
-      params.require(:video).permit(:title, :url)
+      params.require(:video).permit(:title, :url, :description)
     end
 end
