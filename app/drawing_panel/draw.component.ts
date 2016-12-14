96c9991612400e44940f9242eda29b17dd6ec7ca
mikhail <yurkovetc@gmail.com>
2016-12-15 01:18:19 +0300

    Changed home view

--- app/drawing_panel/draw.component.ts
+++ app/drawing_panel/draw.component.ts
@@ -13,12 +13,12 @@ export class DrawComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     ['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f', '#000', '#fff'].forEach(function(i, arr) {
-      document.getElementById('colors_panel').insertAdjacentHTML('beforeend', "<a href='#colors_sketch' data-color='" + i + "' style='width: 15px;height: 15px;display: block;margin: 10px;background: " + i + ";'></a> ");
+      document.getElementById('colors_panel').insertAdjacentHTML('beforeend', "<a href='#colors_sketch' data-color='" + i + "' style='width: 30px;height: 30px;display: block;margin: 10px;background: " + i + ";'></a> ");
     });
     [3, 5, 10, 15].forEach(function(i, arr) {
       document.getElementById('tools').insertAdjacentHTML('beforeend', "<a href='#colors_sketch' data-size='" + i + "' style='background: #ccc'>" + i + "</a> ");
     });
-    $('#colors_sketch').sketch();
+    //$('#colors_sketch').sketch();
   }
 }
 
